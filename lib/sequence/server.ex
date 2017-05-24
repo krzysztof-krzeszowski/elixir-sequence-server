9aaeb4d013a62d5e5f73acfaf053a1b5bd90b286
Krzysztof Krzeszowski <krzysztof.krzeszowski@gmail.com>
2017-05-24 15:02:30 +0200

    :pop call

--- lib/sequence/server.ex
+++ lib/sequence/server.ex
@@ -4,4 +4,11 @@ defmodule Sequence.Server do
   def handle_call(:next_number, _from, current_number) do
     { :reply, current_number, current_number + 1 }
   end
+
+  def handle_call(:pop, _from, []) do
+    { :reply, :empty, [] }
+  end
+  def handle_call(:pop, _from, [h | t]) do
+    { :reply, h, t }
+  end
 end
