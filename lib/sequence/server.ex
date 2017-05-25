4d479c555f05a1abba9a8758f97699c6051d3c72
Krzysztof Krzeszowski <krzysztof.krzeszowski@gmail.com>
2017-05-25 12:09:27 +0200

    removed list implementation

--- lib/sequence/server.ex
+++ lib/sequence/server.ex
@@ -14,33 +14,16 @@ defmodule Sequence.Server do
     GenServer.cast(__MODULE__, {:increment_number, delta})
   end
 
-  def pop do
-    GenServer.call(__MODULE__, :pop)
-  end
-  
-  def push(val) do
-    GenServer.cast(__MODULE__, {:push, val})
-  end
-
   # implementation
   def init stash_pid do
     current_number = Sequence.Stash.get_value stash_pid
     { :ok, {current_number, stash_pid} }
   end
+
   def handle_call(:next_number, _from, {current_number, stash_pid}) do
     { :reply, current_number, {current_number + 1, stash_pid} }
   end
 
-  def handle_call(:pop, _from, {[], stash_pid}) do
-    { :reply, :empty, {[], stash_pid} }
-  end
-  def handle_call(:pop, _from, {[h | t], stash_pid}) do
-    { :reply, h, {t, stash_pid} }
-  end
-
-  def handle_cast({:push, val}, {list, stash_pid}) do
-    { :noreply, {[val | list], stash_pid} }
-  end
 
   def handle_cast({:increment_number, delta}, {current_number, stash_pid}) do
     { :noreply, {current_number + delta, stash_pid} }
